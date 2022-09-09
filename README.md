908ddee595e1d13723b98db88753ac67496f1ecb
Gabriel <grsouza135@gmail.com>
2022-09-08 21:54:55 -0300

    refactor: adding try catch to route post

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # Simple API made with node js and express to upload files to imgur
 
-## This api return the link of the image uploaded to the cloud, so you can use it in any application on the web you want!
+### This api returns the link of the image uploaded to the cloud, so you can use it in any application on the web you want!
 
+### <strong>Main Route:</strong> 
+
+<strong>endpoint:</strong>  /upload/
+
+- request example:
+
+file: 
+"sampleFile": "file"
+
+<br>
+
+- response example:
+
+```
+{
+  "image_url": "https://i.imgur.com/bw9VM3i.png"
+}
+```
--- index.js
+++ index.js
@@ -13,28 +13,32 @@ const client = new ImgurClient({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
-app.use(fileUpload())
+app.use(fileUpload());
 app.use(express.json());
-app.post("/upload/", async(req, res) => {
 
-  if (!req.files) {
-    return res.status(400).json({ message: "No files were uploaded" });
+
+app.get("/",(req,res)=>res.send("App rodando"))
+
+app.post("/upload/", async (req, res) => {
+
+  try{
+    if (!req.files)
+      return res.status(400).json({ message: "No files were uploaded" });
+  
+    let sampleFile = req.files.sampleFile;
+    let uploadPath = __dirname + "/upload/" + sampleFile.name;
+    sampleFile.mv(uploadPath, (err) => {
+      if (err) return res.status(500).json({ message: err });
+    });
+    const response = await client.upload({
+      image: createReadStream(uploadPath),
+      type: "stream",
+    });
+    return res.status(200).json({ image_url: response.data.link });
+
+  }catch(err){
+      if(!req.files?.name) res.status(500).json({ message: "error" });
   }
-  if(!req.files?.name) res.status(400).json({ message: "File Must be named sampleFile" });
-
-  let sampleFile = req.files.sampleFile;
-  let uploadPath = __dirname + "/upload/" + sampleFile.name;
-
-  sampleFile.mv(uploadPath, (err) => {  
-    if (err) return res.status(500).json({message:err});
-  });
-
-  const response = await client.upload({
-    image: createReadStream(uploadPath),
-    type: 'stream',
-  });
-
-  return res.status(200).json({image_url:response.data.link})
 });
 
 app.listen(port, () =>
